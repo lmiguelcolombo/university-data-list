1b5ba13b3a2ab0869fe60965fc767dc42fd12c50
Luís Miguel Colombo <lmiguelcolombo@gmail.com>
2023-11-14 11:48:09 -0300

    feat: usign public API to featch the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function UniversitySearch() {
   const [universities, setUniversities] = useState([]);
+  const [name, setName] = useState("");
+  const [country, setCountry] = useState("");
+
+  const searchUniversities = async (name, country) => {
+    const response = await axios.get(
+      `http://universities.hipolabs.com/search?name=${name}&country=${country}`
+    );
+    setUniversities(response.data);
+  };
+
+  const handleChangeName = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleChangeCountry = (event) => {
+    setCountry(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    searchUniversities(name, country);
+  };
 
   return (
     <div className="container">
